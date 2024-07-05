93be3540bcd6d4edfbd5e9e4d204f4661bb59b24
Asim Ali <hi@asimali.net>
2024-07-05 16:19:53 +0500

    initial project setup & cleanings

--- src/app.js
+++ src/app.js
@@ -1,21 +1,18 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const {sequelize} = require('./model')
-const {getProfile} = require('./middleware/getProfile')
-const app = express();
-app.use(bodyParser.json());
-app.set('sequelize', sequelize)
-app.set('models', sequelize.models)
+const express        = require('express');
+const { sequelize }  = require('./model');
+const { getProfile } = require('./middleware');
+const app            = express();
+app.use(express.json());
+app.set('sequelize', sequelize);
+app.set('models', sequelize.models);
 
-/**
- * FIX ME!
- * @returns contract by id
- */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
-    if(!contract) return res.status(404).end()
-    res.json(contract)
-})
+
+app.get('/contracts/:id', getProfile, async (req, res) => {
+  const { Contract } = req.app.get('models');
+  const { id }       = req.params;
+  const contract     = await Contract.findOne({ where: { id } });
+  if (!contract) return res.status(404).end();
+  res.json(contract);
+});
+
 module.exports = app;
--- src/model.js
+++ src/model.js
@@ -2,34 +2,34 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: './database.sqlite3'
+  storage: './database.sqlite3',
 });
 
 class Profile extends Sequelize.Model {}
 Profile.init(
   {
     firstName: {
-      type: Sequelize.STRING,
-      allowNull: false
+      type     : Sequelize.STRING,
+      allowNull: false,
     },
     lastName: {
-      type: Sequelize.STRING,
-      allowNull: false
+      type     : Sequelize.STRING,
+      allowNull: false,
     },
     profession: {
-      type: Sequelize.STRING,
-      allowNull: false
+      type     : Sequelize.STRING,
+      allowNull: false,
     },
-    balance:{
-      type:Sequelize.DECIMAL(12,2)
+    balance: {
+      type: Sequelize.DECIMAL(12, 2),
     },
     type: {
-      type: Sequelize.ENUM('client', 'contractor')
-    }
+      type: Sequelize.ENUM('client', 'contractor'),
+    },
   },
   {
     sequelize,
-    modelName: 'Profile'
+    modelName: 'Profile',
   }
 );
 
@@ -37,16 +37,16 @@ class Contract extends Sequelize.Model {}
 Contract.init(
   {
     terms: {
-      type: Sequelize.TEXT,
-      allowNull: false
+      type     : Sequelize.TEXT,
+      allowNull: false,
     },
-    status:{
-      type: Sequelize.ENUM('new','in_progress','terminated')
-    }
+    status: {
+      type: Sequelize.ENUM('new', 'in_progress', 'terminated'),
+    },
   },
   {
     sequelize,
-    modelName: 'Contract'
+    modelName: 'Contract',
   }
 );
 
@@ -54,37 +54,37 @@ class Job extends Sequelize.Model {}
 Job.init(
   {
     description: {
-      type: Sequelize.TEXT,
-      allowNull: false
+      type     : Sequelize.TEXT,
+      allowNull: false,
     },
-    price:{
-      type: Sequelize.DECIMAL(12,2),
-      allowNull: false
+    price: {
+      type     : Sequelize.DECIMAL(12, 2),
+      allowNull: false,
     },
     paid: {
-      type: Sequelize.BOOLEAN,
-      default:false
+      type   : Sequelize.BOOLEAN,
+      default: false,
     },
-    paymentDate:{
-      type: Sequelize.DATE
-    }
+    paymentDate: {
+      type: Sequelize.DATE,
+    },
   },
   {
     sequelize,
-    modelName: 'Job'
+    modelName: 'Job',
   }
 );
 
-Profile.hasMany(Contract, {as :'Contractor',foreignKey:'ContractorId'})
-Contract.belongsTo(Profile, {as: 'Contractor'})
-Profile.hasMany(Contract, {as : 'Client', foreignKey:'ClientId'})
-Contract.belongsTo(Profile, {as: 'Client'})
-Contract.hasMany(Job)
-Job.belongsTo(Contract)
+Profile.hasMany(Contract, { as: 'Contractor', foreignKey: 'ContractorId' });
+Contract.belongsTo(Profile, { as: 'Contractor' });
+Profile.hasMany(Contract, { as: 'Client', foreignKey: 'ClientId' });
+Contract.belongsTo(Profile, { as: 'Client' });
+Contract.hasMany(Job);
+Job.belongsTo(Contract);
 
 module.exports = {
   sequelize,
   Profile,
   Contract,
-  Job
+  Job,
 };
